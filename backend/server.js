850af507e09d2c5366569c5b9667b81b277c2620
effham.ali <athereffham@gmail.com>
2023-07-14 21:41:52 +0500

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,47 @@
-const express = require('express')
-const colors = require('colors')
-const dotenv = require('dotenv').config()
+const path = require("path");
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
 
-const {errorHandler} = require('./middleware/errorMiddleware')
+const { errorHandler } = require("./middleware/errorMiddleware");
 
-const connectDB = require('./config/db') 
+const connectDB = require("./config/db");
 
-const PORT = process.env.PORT || 8000 
+const PORT = process.env.PORT || 8000;
 
+connectDB();
 
-connectDB()
+const app = express();
 
-const app = express()
+app.use(express.json());
 
-app.use(express.json())
-
-app.use(express.urlencoded({
-    extended:false
-}))
-
-
-app.get('/',(req,res) => {
-    res.json({message:'Hello',hello:"CHECK"})
-})
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 
 // ROUTES
 
-app.use('/api/users',require('./routes/userRoutes'))
+app.use("/api/users", require("./routes/userRoutes"));
 
-app.use('/api/tickets',require('./routes/ticketRoutes'))
+app.use("/api/tickets", require("./routes/ticketRoutes"));
 
+// SERVE PRODUCTION
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.use(errorHandler)
+  app.get("*", (req, res) =>
+    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.json({ message: "Hello", hello: "CHECK" });
+  });
+}
 
+app.use(errorHandler);
 
-
-app.listen(PORT,() => {
-    console.log(`server started on port ${PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
